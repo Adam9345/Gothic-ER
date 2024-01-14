e18c9820ef52ae8ddc5303984d5a547c007dbf04
Reflide <refidemodder@gmail.com>
2024-01-14 23:18:13 +0100

    Poprawki w dialogach

--- Content/Story/Missions/DIA__MOD_ORG_807_Organisator_Graver_Zaklady.d
+++ Content/Story/Missions/DIA__MOD_ORG_807_Organisator_Graver_Zaklady.d
@@ -47,10 +47,10 @@ FUNC INT DIA_Organisator_HELLO1_Condition()
 };
 FUNC VOID DIA_Organisator_HELLO1_Info()
 {
-    AI_Output (self, other ,"DIA_Organisator_HELLO1_03_01"); //Hej! Dorabiam sobie na boku organizuj¹c ma³e zak³ady.
-    AI_Output (self, other ,"DIA_Organisator_HELLO1_03_02"); //Chcesz coœ obstawiæ? A mo¿e wolisz zagraæ w koœci?
+    AI_Output (self, other ,"DIA_Organisator_HELLO1_03_01"); //Hej! Dorabiam sobie na boku organizujÄ…c maÅ‚e zakÅ‚ady.
+    AI_Output (self, other ,"DIA_Organisator_HELLO1_03_02"); //Chcesz coÅ› obstawiÄ‡? A moÅ¼e wolisz zagraÄ‡ w koÅ›ci?
 	Log_CreateTopic   	(GE_Games,LOG_NOTE);
-    B_LogEntry          (GE_Games,"Szkodnik imieniem Greaver oferuje grê w koœci i proste zak³ady. Je¿eli mam chêæ zagraæ, muszê siê do niego zg³osiæ.");
+    B_LogEntry          (GE_Games,"Szkodnik imieniem Greaver oferuje grÄ™ w koÅ›ci i proste zakÅ‚ady. JeÅ¼eli mam chÄ™Ä‡ zagraÄ‡, muszÄ™ siÄ™ do niego zgÅ‚osiÄ‡.");
 };
 
 //========================================
@@ -64,7 +64,7 @@ INSTANCE DIA_Organisator_ZkladyInfos (C_INFO)
    condition    = DIA_Organisator_ZkladyInfos_Condition;
    information  = DIA_Organisator_ZkladyInfos_Info;
    permanent	= 0;
-   description	= "Na czym polegaj¹ zak³ady?";
+   description	= "Na czym polegajÄ… zakÅ‚ady?";
 };
 
 FUNC INT DIA_Organisator_ZkladyInfos_Condition()
@@ -78,9 +78,9 @@ FUNC INT DIA_Organisator_ZkladyInfos_Condition()
 
 FUNC VOID DIA_Organisator_ZkladyInfos_Info()
 {
-    AI_Output (other, self ,"DIA_Organisator_ZkladyInfos_15_01"); //Na czym polegaj¹ zak³ady?
-    AI_Output (self, other ,"DIA_Organisator_ZkladyInfos_03_02"); //To proste. Obstawiamy 50 bry³ek rudy na to, kogo dzisiaj pobije Butch.
-    AI_Output (other, self ,"DIA_Organisator_ZkladyInfos_15_03"); //Dosyæ oryginalne.
+    AI_Output (other, self ,"DIA_Organisator_ZkladyInfos_15_01"); //Na czym polegajÄ… zakÅ‚ady?
+    AI_Output (self, other ,"DIA_Organisator_ZkladyInfos_03_02"); //To proste. Obstawiamy 50 bryÅ‚ek rudy na to, kogo dzisiaj pobije Butch.
+    AI_Output (other, self ,"DIA_Organisator_ZkladyInfos_15_03"); //DosyÄ‡ oryginalne.
 };
 
 //========================================
@@ -94,7 +94,7 @@ INSTANCE DIA_Organisator_Obstawiaj (C_INFO)
    condition    = DIA_Organisator_Obstawiaj_Condition;
    information  = DIA_Organisator_Obstawiaj_Info;
    permanent	= TRUE;
-   description	= "Chcê obstawiæ...";
+   description	= "ChcÄ™ obstawiÄ‡...";
 };
 
 FUNC INT DIA_Organisator_Obstawiaj_Condition()
@@ -108,7 +108,7 @@ FUNC INT DIA_Organisator_Obstawiaj_Condition()
 
 FUNC VOID DIA_Organisator_Obstawiaj_Info()
 {
-    AI_Output (other, self ,"DIA_Organisator_Obstawiaj_15_01"); //Chcê obstawiæ...
+    AI_Output (other, self ,"DIA_Organisator_Obstawiaj_15_01"); //ChcÄ™ obstawiÄ‡...
 
     Info_ClearChoices		(DIA_Organisator_Obstawiaj);
     Info_AddChoice		(DIA_Organisator_Obstawiaj, DIALOG_BACK, DIA_Organisator_Obstawiaj_BACK);
@@ -129,11 +129,11 @@ FUNC VOID DIA_Organisator_Obstawiaj_BACK()
 
 FUNC VOID DIA_Organisator_Obstawiaj_Baal()
 {
-AI_Output (other, self ,"DIA_Organisator_Obstawiaj_Baal_15_01"); //Dziœ dostanie Baal Kagan.
+AI_Output (other, self ,"DIA_Organisator_Obstawiaj_Baal_15_01"); //DziÅ› dostanie Baal Kagan.
 gra4 = Hlp_Random (2);
 if (gra4 == true)
 {
-		AI_Output (self, other ,"DIA_Organisator_Obstawiaj_Baal_03_01"); //Ho, ho! NieŸle!
+		AI_Output (self, other ,"DIA_Organisator_Obstawiaj_Baal_03_01"); //Ho, ho! NieÅºle!
 
 		Npc_SetTarget(Org_851_Butch,Nov_1332_BaalKagan);
 		AI_StartState(Org_851_Butch,ZS_ATTACK,1,"");
@@ -142,7 +142,7 @@ if (gra4 == true)
 		}
 		else 
 		{
-		AI_Output (self, other ,"DIA_Organisator_Obstawiaj_Baal_03_02"); //No, nie uda³o siê!
+		AI_Output (self, other ,"DIA_Organisator_Obstawiaj_Baal_03_02"); //No, nie udaÅ‚o siÄ™!
 		B_GiveInvItems (other, self, ItMiNugget, 50);
 		Npc_RemoveInvItems (self, ItMiNugget, 50);
 		Npc_SetTarget(Org_851_Butch,ORG_817_Organisator);
@@ -153,11 +153,11 @@ AI_StopProcessInfos	(self);
 
 FUNC VOID DIA_Organisator_Obstawiaj_Handlarz()
 {
-AI_Output (other, self ,"DIA_Organisator_Obstawiaj_Handlarz_15_01"); //Dziœ dostanie Szkodnik przy kotle.
+AI_Output (other, self ,"DIA_Organisator_Obstawiaj_Handlarz_15_01"); //DziÅ› dostanie Szkodnik przy kotle.
 gra4 = Hlp_Random (2);
 if (gra4 == true)
 {
-		AI_Output (self, other ,"DIA_Organisator_Obstawiaj_Handlarz_03_01"); //Ho, ho! NieŸle!
+		AI_Output (self, other ,"DIA_Organisator_Obstawiaj_Handlarz_03_01"); //Ho, ho! NieÅºle!
 		Npc_SetTarget(Org_851_Butch,ORG_817_Organisator);
 		AI_StartState(Org_851_Butch,ZS_ATTACK,1,"");
 		CreateInvItems (self, ItMiNugget, 50);
@@ -165,7 +165,7 @@ if (gra4 == true)
 		}
 		else 
 		{
-		AI_Output (self, other ,"DIA_Organisator_Obstawiaj_Handlarz_03_02"); //No, nie uda³o siê!
+		AI_Output (self, other ,"DIA_Organisator_Obstawiaj_Handlarz_03_02"); //No, nie udaÅ‚o siÄ™!
 		B_GiveInvItems (other, self, ItMiNugget, 50);
 		Npc_RemoveInvItems (self, ItMiNugget, 50);
 		Npc_SetTarget(Org_851_Butch,Nov_1332_BaalKagan);
@@ -176,11 +176,11 @@ AI_StopProcessInfos	(self);
 
 FUNC VOID DIA_Organisator_Obstawiaj_Ktos1()
 {
-AI_Output (other, self ,"DIA_Organisator_Obstawiaj_Ktos1_15_01"); //Dziœ dostanie Szkodnik przy ognisku.
+AI_Output (other, self ,"DIA_Organisator_Obstawiaj_Ktos1_15_01"); //DziÅ› dostanie Szkodnik przy ognisku.
 gra4 = Hlp_Random (2);
 if (gra4 == true)
 {
-		AI_Output (self, other ,"DIA_Organisator_Obstawiaj_Ktos1_03_01"); //Ho, ho! NieŸle!
+		AI_Output (self, other ,"DIA_Organisator_Obstawiaj_Ktos1_03_01"); //Ho, ho! NieÅºle!
 		Npc_SetTarget(Org_851_Butch,ORG_822_Organisator);
 		AI_StartState(Org_851_Butch,ZS_ATTACK,1,"");
 		CreateInvItems (self, ItMiNugget, 50);
@@ -188,7 +188,7 @@ if (gra4 == true)
 		}
 		else 
 		{
-		AI_Output (self, other ,"DIA_Organisator_Obstawiaj_Ktos1_03_02"); //No, nie uda³o siê!
+		AI_Output (self, other ,"DIA_Organisator_Obstawiaj_Ktos1_03_02"); //No, nie udaÅ‚o siÄ™!
 		B_GiveInvItems (other, self, ItMiNugget, 50);
 		Npc_RemoveInvItems (self, ItMiNugget, 50);
 		Npc_SetTarget(Org_851_Butch,Nov_1332_BaalKagan);
@@ -199,11 +199,11 @@ AI_StopProcessInfos	(self);
 
 FUNC VOID DIA_Organisator_Obstawiaj_Ktos2()
 {
-AI_Output (other, self ,"DIA_Organisator_Obstawiaj_Ktos2_15_01"); //Dziœ dostanie Krzykacz!
+AI_Output (other, self ,"DIA_Organisator_Obstawiaj_Ktos2_15_01"); //DziÅ› dostanie Krzykacz!
 gra4 = Hlp_Random (2);
 if (gra4 == true)
 {
-		AI_Output (self, other ,"DIA_Organisator_Obstawiaj_Ktos2_03_01"); //Trafi³eœ!
+		AI_Output (self, other ,"DIA_Organisator_Obstawiaj_Ktos2_03_01"); //TrafiÅ‚eÅ›!
 		Npc_SetTarget(Org_851_Butch,ORG_842_Shrike);
 		AI_StartState(Org_851_Butch,ZS_ATTACK,1,"");
 		CreateInvItems (self, ItMiNugget, 50);
@@ -211,7 +211,7 @@ if (gra4 == true)
 		}
 		else 
 		{
-		AI_Output (self, other ,"DIA_Organisator_Obstawiaj_Ktos2_03_02"); //No, nie uda³o siê!
+		AI_Output (self, other ,"DIA_Organisator_Obstawiaj_Ktos2_03_02"); //No, nie udaÅ‚o siÄ™!
 		B_GiveInvItems (other, self, ItMiNugget, 50);
 		Npc_RemoveInvItems (self, ItMiNugget, 50);
 		Npc_SetTarget(Org_851_Butch,ORG_822_Organisator);
@@ -236,7 +236,7 @@ INSTANCE DIA_Organisator_HELLO3 (C_INFO)
    condition    = DIA_Organisator_HELLO3_Condition;
    information  = DIA_Organisator_HELLO3_Info;
    permanent	= TRUE;
-   description	= "Zagrajmy w koœci.";
+   description	= "Zagrajmy w koÅ›ci.";
 };
 
 FUNC INT DIA_Organisator_HELLO3_Condition()
@@ -249,18 +249,18 @@ FUNC INT DIA_Organisator_HELLO3_Condition()
 
 func void dia_organisator_hello3_choices() {
 	if(npc_hasitems(hero, itminugget) >= 5) {
-        info_addchoice(dia_organisator_hello3, "5 bry³ek rudy", dia_organisator_hello3_20ore);
+        info_addchoice(dia_organisator_hello3, "5 bryÅ‚ek rudy", dia_organisator_hello3_20ore);
     };
     if(npc_hasitems(hero, itminugget) >= 15) {
-        info_addchoice(dia_organisator_hello3, "15 bry³ek rudy", dia_organisator_hello3_60ore);
+        info_addchoice(dia_organisator_hello3, "15 bryÅ‚ek rudy", dia_organisator_hello3_60ore);
     };
     if(npc_hasitems(hero, itminugget) >= 30) {
-        info_addchoice(dia_organisator_hello3, "30 bry³ek rudy", dia_organisator_hello3_150ore);
+        info_addchoice(dia_organisator_hello3, "30 bryÅ‚ek rudy", dia_organisator_hello3_150ore);
     };
     if(npc_hasitems(hero, itminugget) >= 50) {
-        info_addchoice(dia_organisator_hello3, "50 bry³ek rudy", dia_organisator_hello3_300ore);
+        info_addchoice(dia_organisator_hello3, "50 bryÅ‚ek rudy", dia_organisator_hello3_300ore);
     };
-    info_addchoice(dia_organisator_hello3, "Jednak zrezygnujê.", dia_organisator_hello3_back);
+    info_addchoice(dia_organisator_hello3, "Jednak zrezygnujÄ™.", dia_organisator_hello3_back);
 };
 
 func void dia_organisator_hello3_info() {
@@ -297,47 +297,47 @@ func void dia_organisator_hello3_300ore() {
 
 FUNC VOID DIA_Organisator_HELLO3_500Ore()
 {
-    AI_Output (other, self ,"DIA_Organisator_HELLO3_500Ore_15_01"); //A co mi tam! 500 bry³ek.
-    AI_Output (self, other ,"DIA_Organisator_HELLO3_500Ore_03_02"); //No nieŸle! 
+    AI_Output (other, self ,"DIA_Organisator_HELLO3_500Ore_15_01"); //A co mi tam! 500 bryÅ‚ek.
+    AI_Output (self, other ,"DIA_Organisator_HELLO3_500Ore_03_02"); //No nieÅºle! 
     B_GiveXP (150);
 	
 gra1 = Hlp_Random (7);
 if (gra1 == 1)
 {
-AI_Output (self, other ,"DIA_Organisator_HELLO3_LOSE"); //No niestety. Nie uda³o siê. Ruda trafia do mnie!
+AI_Output (self, other ,"DIA_Organisator_HELLO3_LOSE"); //No niestety. Nie udaÅ‚o siÄ™. Ruda trafia do mnie!
 B_GiveInvItems (other, self, ItMiNugget, 500);
 };
 if (gra1 == 2)
 {
-Print ("Wygra³eœ!");
+Print ("WygraÅ‚eÅ›!");
 CreateInvItems (self, ItMiNugget, 500);
 B_GiveInvItems (self, other, ItMiNugget, 500);
 };
 if (gra1 == 3)
 {
-AI_Output (self, other ,"DIA_Organisator_HELLO3_LOSE"); //No niestety. Nie uda³o siê. Ruda trafia do mnie!
+AI_Output (self, other ,"DIA_Organisator_HELLO3_LOSE"); //No niestety. Nie udaÅ‚o siÄ™. Ruda trafia do mnie!
 B_GiveInvItems (other, self, ItMiNugget, 500);
 };
 if (gra1 == 4)
 {
-Print ("Wygra³eœ!");
+Print ("WygraÅ‚eÅ›!");
 CreateInvItems (self, ItMiNugget, 500);
 B_GiveInvItems (self, other, ItMiNugget, 500);
 };
 if (gra1 == 5)
 {
-Print ("Wygra³eœ!");
+Print ("WygraÅ‚eÅ›!");
 CreateInvItems (self, ItMiNugget, 500);
 B_GiveInvItems (self, other, ItMiNugget, 500);
 };
 if (gra1 == 6)
 {
-AI_Output (self, other ,"DIA_Organisator_HELLO3_LOSE"); //No niestety. Nie uda³o siê. Ruda trafia do mnie!
+AI_Output (self, other ,"DIA_Organisator_HELLO3_LOSE"); //No niestety. Nie udaÅ‚o siÄ™. Ruda trafia do mnie!
 B_GiveInvItems (other, self, ItMiNugget, 500);
 };
 if (gra1 == 7)
 {
-AI_Output (self, other ,"DIA_Organisator_HELLO3_LOSE"); //No niestety. Nie uda³o siê. Ruda trafia do mnie!
+AI_Output (self, other ,"DIA_Organisator_HELLO3_LOSE"); //No niestety. Nie udaÅ‚o siÄ™. Ruda trafia do mnie!
 B_GiveInvItems (other, self, ItMiNugget, 500);
 };
 };
@@ -373,19 +373,19 @@ if (Npc_KnowsInfo(hero,DIA_alvaro_ANDREW))
 FUNC VOID DIA_Organisator_ANDREW_Info()
 {
     AI_Output (other, self ,"DIA_Organisator_ANDREW_15_01"); //Gadaj gdzie jest Sharky!
-    AI_Output (self, other ,"DIA_Organisator_ANDREW_03_02"); //Nie wiem. Nie jestem jego niañk¹.
-    AI_Output (other, self ,"DIA_Organisator_ANDREW_15_03"); //Skoro wymieniliœmy siê ju¿ uprzejmoœciami to radzi³bym zachowaæ wiêkszy rozs¹dek.
-    AI_Output (other, self ,"DIA_Organisator_ANDREW_15_04"); //Inaczej bêdzie powtórka!
+    AI_Output (self, other ,"DIA_Organisator_ANDREW_03_02"); //Nie wiem. Nie jestem jego niaÅ„kÄ….
+    AI_Output (other, self ,"DIA_Organisator_ANDREW_15_03"); //Skoro wymieniliÅ›my siÄ™ juÅ¼ uprzejmoÅ›ciami, to radziÅ‚bym zachowaÄ‡ wiÄ™kszy rozsÄ…dek.
+    AI_Output (other, self ,"DIA_Organisator_ANDREW_15_04"); //Chyba, Å¼e chcesz powtÃ³rzyÄ‡ precedens. 
     AI_DrawWeapon (other);
-    AI_Output (self, other ,"DIA_Organisator_ANDREW_03_05"); //Dobra zostaw mnie w spokoju a w zamian dowiesz siê gdzie on jest.
-    AI_Output (other, self ,"DIA_Organisator_ANDREW_15_06"); //Jednak potrafisz myœleæ. Zamieniam siê w s³uch.
+    AI_Output (self, other ,"DIA_Organisator_ANDREW_03_05"); //Dobra, juÅ¼ dobra! Powiem Ci, gdzie on jest.
+    AI_Output (other, self ,"DIA_Organisator_ANDREW_15_06"); //Jednak potrafisz myÅ›leÄ‡. Zamieniam siÄ™ w sÅ‚uch.
     AI_RemoveWeapon (other);
-    AI_Output (self, other ,"DIA_Organisator_ANDREW_03_07"); //Poszed³ do myœliwego Aidana nieopodal. Tamten pozyskuje sporo trofeów myœliwskich.
-    AI_Output (self, other ,"DIA_Organisator_ANDREW_03_08"); //Wiêc mo¿e Sharky jest tym zainteresowany. Cholera go wie.
-    AI_Output (other, self ,"DIA_Organisator_ANDREW_15_09"); //Dziêki za informacje. Nastêpnym razem nie krzy¿uj ze mn¹ orê¿a.
-    AI_Output (self, other ,"DIA_Organisator_ANDREW_03_10"); //To wina tego chojraka, który wys³uguje siê innymi. Wracam do Nowego Obozu liczyæ siniaki.
-    AI_Output (other, self ,"DIA_Organisator_ANDREW_15_11"); //Na razie i przyjemnego liczenia...
-	 B_LogEntry                     (CH1_Bodyguard,"Pokonaliœmy zbirów, teraz dziêki informacjom od Greavera musimy przeprowadziæ powa¿n¹ rozmowê z Sharky'm. Ponoæ obecnie jest u myœliwego Aidana.");
+    AI_Output (self, other ,"DIA_Organisator_ANDREW_03_07"); //PoszedÅ‚ do myÅ›liwego Aidana, nieopodal. GoÅ›Ä‡ ma sporo trofeÃ³w myÅ›liwskich.
+    AI_Output (self, other ,"DIA_Organisator_ANDREW_03_08"); //WiÄ™c moÅ¼e Sharky jest tym zainteresowany. Cholera go wie.
+    AI_Output (other, self ,"DIA_Organisator_ANDREW_15_09"); //DziÄ™ki za informacje. I wybacz za twÃ³j stan. 
+    AI_Output (self, other ,"DIA_Organisator_ANDREW_03_10"); //To wina tego chojraka, ktÃ³ry wysÅ‚uguje siÄ™ innymi. Wracam do Nowego Obozu liczyÄ‡ siniaki.
+    AI_Output (other, self ,"DIA_Organisator_ANDREW_15_11"); //Trzymaj siÄ™.
+	 B_LogEntry                     (CH1_Bodyguard,"PokonaliÅ›my zbirÃ³w, teraz dziÄ™ki informacjom od Greavera musimy przeprowadziÄ‡ powaÅ¼nÄ… rozmowÄ™ z Sharky'm. PonoÄ‡ obecnie jest u myÅ›liwego Aidana.");
 	AI_StopProcessInfos	(self);
 };
 
