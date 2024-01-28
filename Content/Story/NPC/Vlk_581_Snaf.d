0e0c4bdec9ae805f6ab791b3e18c3e7d5502378d
Adam9345 <adam9345@github.com>
2024-01-28 14:29:46 +0100

    Poprawiono Snafowi rutynę spania

--- Content/Story/NPC/Vlk_581_Snaf.d
+++ Content/Story/NPC/Vlk_581_Snaf.d
@@ -46,7 +46,7 @@ instance VLK_581_Snaf (Npc_Default)
 FUNC VOID Rtn_start_581 ()
 {
     TA_Cook		(08,00,22,00,"OCR_CAULDRON_2");
-	TA_Sleep		(22,00,08,00,"OCR_CAULDRON_2");
+	TA_Sleep		(22,00,08,00,"OCR_HUT_14");
 };
 FUNC VOID Rtn_nockruka_581 ()
 {
